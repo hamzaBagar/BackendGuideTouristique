f96d096039e4c8767e4730f5e1efa5f74e01a62f
ElbagarHamza <elbagar.hamza@gmail.com>
2021-06-06 20:26:54 +0100

    first commit

--- src/main/java/org/mql/android/modele/Location.java
+++ src/main/java/org/mql/android/modele/Location.java
@@ -1,9 +1,37 @@
 package org.mql.android.modele;
 
-public class Location {
+import org.bson.types.ObjectId;
 
+public class Location {
+	private ObjectId id;
+	private String cityName;
+	private String description;
+	private String picture;
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName.toUpperCase();
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
 	public Location() {
-		// TODO Auto-generated constructor stub
 	}
 
 }
